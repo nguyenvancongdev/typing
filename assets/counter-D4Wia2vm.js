b947ba838be8b807212d600a0f6b9e674fc3078d
Nguyen Văn Cong <congnv@etc.vn>
2024-07-19 13:53:44 +0700

    deploy

--- assets/counter-D4Wia2vm.js
+++
@@ -1,1 +0,0 @@
-import{i as o,e as n,f as r}from"./index-wG2_gqcC.js";const s=o("counter",()=>{const e=n(0),t=r(()=>e.value*2);return{count:e,doubleCount:t,increment:()=>{e.value++}}});export{s as u};
